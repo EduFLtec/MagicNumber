b4c99285ec3e9e3c2e0e2a7c06089a86fe674841
K.Korey Wertman <kkwertman@mail.fhsu.edu>
2021-04-07 17:10:05 -0400

    Finish game, next reload issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let isNumberFound = false;
 let guesses = 0;
 
 // UI Variables
-const  gameWrapper = document.querySelector('.game');
+const gameWrapper = document.querySelector('#game-container');
 const minNum = document.querySelector('.min-num');
 const maxNum = document.querySelector('.max-num');
 const guessInput = document.querySelector("#guess-input");
@@ -15,12 +15,18 @@ const message = document.querySelector('.message');
 const higherHint = document.querySelector("#higer-hint");
 const lowerHint = document.querySelector("#lower-hint");
 const guessesCounter = document.querySelector("#guess-counter");
+const gameCard = document.querySelector('#game-card');
 
 // Assign UI min and max 
 minNum.textContent = min;
 maxNum.textContent = max;
 
-
+// Play again option after a win, needs fix
+gameWrapper.addEventListener('mousedown', function(e){
+	if(e.target.className === 'play-again'){
+	  window.location.reload();
+	}
+  });
 
 // Generate winning number
 function winningRandomNumber(min, max) {
@@ -30,21 +36,25 @@ function winningRandomNumber(min, max) {
 
 guessBtn.addEventListener('click', function () {
 	let guess = parseInt(guessInput.value);
+	guessInput.value = '';
 	
 	// Validation
 	if (isNumberFound === false) {
-		if (isNaN(guess) || guess < min || guess > max || guess == "") {
+		if (isNaN(guess) || guess < min || guess > max || guess === "") {
 			setMessage(`Please enter a valid number between ${min} and ${max}`, 'warning');
-		} else if (guess == winningNumber) {
+		} else if (guess === winningNumber) {
 			isNumberFound = true;
 			let stats = `You took ${guesses} guesses to find the magic number!`;
 			setMessage(`${winningNumber} is the magic number! You win! ${stats}`, 'success');
+			gameCard.classList.toggle('border-success');
+			guessBtn.value = 'Play Again';
+			guessBtn.className += ' play-again';
 		} else {
 			guesses = guesses + 1;
 			setGuessCounter();
-			    if (guess < winningNumber) {
+			    if (winningNumber > guess) {
 					setHighHint(`It's higher than: ${guess}`);
-				} else if (guess > winningNumber) {
+				} else {
 					setLowerHint(`It's lower than: ${guess}`);
 				}
 			}
@@ -53,6 +63,7 @@ guessBtn.addEventListener('click', function () {
 	}
 });
 	
+
 // Higher hint
 	function setHighHint(msg) {
 		higherHint.textContent = msg;
